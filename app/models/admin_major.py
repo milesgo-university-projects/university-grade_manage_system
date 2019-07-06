da444cd3f7d234f8cad6ac99c7a50417ffcd81f4
MilesGO <164173218@qq.com>
2019-07-06 22:46:16 +0800

    third commit

--- app/models/admin_major.py
+++ app/models/admin_major.py
@@ -1,9 +1,31 @@
 from app.models.general import connect_to_sql
 
 
+class MajorIdReader:
+    def __init__(self):
+        self.data = read_major_id()
+
+
 class MajorReader:
     def __init__(self):
         self.data = read_major()
+
+
+def read_major_id():
+    connection = connect_to_sql()
+    data = {'major_ids': []}
+    try:
+        with connection.cursor() as cursor:
+            sql = 'select major_id from major'
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            for row in result:
+                data['major_ids'].append(row[0])
+    except Exception as e:
+        data['error'] = str(e)
+    finally:
+        connection.close()
+    return data
 
 
 def read_major():
