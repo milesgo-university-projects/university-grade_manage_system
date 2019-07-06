bb6011c9f02f6eda7965867778c1081e48c44c45
MilesGO <milesgo517@gmail.com>
2019-07-06 11:31:05 +0800

    modify secure_config.py

--- app/models/general.py
+++ app/models/general.py
@@ -1,7 +1,9 @@
 import pymysql
+from flask import current_app
 
 
 def connect_to_sql():
-    connection = pymysql.connect(host='localhost', user='root', password='330501',
+    connection = pymysql.connect(host='localhost', user=current_app.config['DATABASE_USER'],
+                                 password=current_app.config['DATABASE_PASSWORD'],
                                  database='grade_manage_system', charset='utf8')
     return connection
