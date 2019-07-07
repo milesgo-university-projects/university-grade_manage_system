fb2ca859b1c1a29edc4ff8471340fa8df041b0ad
MilesGO <164173218@qq.com>
2019-07-07 12:09:59 +0800

    default

--- app/validate/session.py
+++ app/validate/session.py
@@ -1,4 +1,5 @@
-from wtforms import Form
+from wtforms import Form, StringField
+from wtforms.validators import Regexp
 
 
 class StudentLoginForm(Form):
@@ -6,7 +7,10 @@ class StudentLoginForm(Form):
 
 
 class StudentChangePasswordForm(Form):
-    pass
+    # 学生编号为10位数字
+    student_id = StringField(validators=[Regexp(regex=r'^[0-9]{10}$', message='学生编号应为10位数字')])
+    old_password = StringField(validators=[Regexp(regex=r'^[0-9a-zA-Z]{5,18}$', message='旧密码应为5~18位数字或字母组成')])
+    new_password = StringField(validators=[Regexp(regex=r'^[0-9a-zA-Z]{5,18}$', message='新密码应为5~18位数字或字母组成')])
 
 
 class TeacherLoginForm(Form):
@@ -14,7 +18,10 @@ class TeacherLoginForm(Form):
 
 
 class TeacherChangePasswordForm(Form):
-    pass
+    # 教师编号为5位数字
+    teacher_id = StringField(validators=[Regexp(regex=r'^[0-9]{5}$', message='教师编号应为5位数字')])
+    old_password = StringField(validators=[Regexp(regex=r'^[0-9a-zA-Z]{5,18}$', message='旧密码应为5~18位数字或字母组成')])
+    new_password = StringField(validators=[Regexp(regex=r'^[0-9a-zA-Z]{5,18}$', message='新密码应为5~18位数字或字母组成')])
 
 
 class AdminLoginForm(Form):
