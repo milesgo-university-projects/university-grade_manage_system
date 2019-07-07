bb2893501841ec4883cdd0e32f101dc4a6a4f6a6
MilesGO <milesgo517@gmail.com>
2019-07-07 10:13:40 +0800

    fifth commit

--- app/models/admin_major.py
+++ app/models/admin_major.py
@@ -11,6 +11,109 @@ class MajorReader:
         self.data = read_major()
 
 
+class SingleMajorReader:
+    def __init__(self, major_id):
+        self.data = read_single_major(major_id)
+
+
+class MajorUpdater:
+    def __init__(self, major_id, major_name):
+        self.data = update_major(major_id, major_name)
+
+
+class MajorInserter:
+    def __init__(self, major_name):
+        self.data = insert_major(major_name)
+
+
+class MajorDeleter:
+    def __init__(self, major_id):
+        self.data = delete_major(major_id)
+
+
+def delete_major(major_id):
+    connection = connect_to_sql()
+    data = {}
+    try:
+        with connection.cursor() as cursor:
+            sql = 'delete from major where major_id = \'%s\';' % major_id
+            cursor.execute(sql)
+            connection.commit()
+    except Exception as e:
+        data['error'] = str(e)
+    finally:
+        connection.close()
+    return data
+
+
+def get_new_major_id():
+    connection = connect_to_sql()
+    data = {}
+    try:
+        with connection.cursor() as cursor:
+            sql = 'select max(convert(major_id, unsigned integer))+1 from major order by major_id'
+            cursor.execute(sql)
+            data['major_id'] = str(cursor.fetchone()[0])
+            while len(data['major_id']) < 3:
+                data['major_id'] = '0' + data['major_id']
+    except Exception as e:
+        data['error'] = str(e)
+    finally:
+        connection.close()
+    return data
+
+
+def insert_major(major_name):
+    connection = connect_to_sql()
+    data = {}
+    try:
+        with connection.cursor() as cursor:
+            major_id = get_new_major_id()
+            if major_id.get('error'):
+                data['error'] = major_id['error']
+                return data
+            sql = 'insert into major values(\'%s\', \'%s\');' % (major_id['major_id'], major_name)
+            cursor.execute(sql)
+            connection.commit()
+    except Exception as e:
+        data['error'] = str(e)
+    finally:
+        connection.close()
+    return data
+
+
+def update_major(major_id, major_name):
+    connection = connect_to_sql()
+    data = {}
+    try:
+        with connection.cursor() as cursor:
+            sql = 'update major set major_name = \'%s\' where major_id = \'%s\';' % (major_name, major_id)
+            cursor.execute(sql)
+            connection.commit()
+    except Exception as e:
+        data['error'] = str(e)
+    finally:
+        connection.close()
+    return data
+
+
+def read_single_major(major_id):
+    connection = connect_to_sql()
+    data = {}
+    try:
+        with connection.cursor() as cursor:
+            sql = 'select major_id, major_name from major where major_id = \'%s\';' % major_id
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            data['major_id'] = result[0]
+            data['major_name'] = result[1]
+    except Exception as e:
+        data['error'] = str(e)
+    finally:
+        connection.close()
+    return data
+
+
 def read_major_id():
     connection = connect_to_sql()
     data = {'major_ids': []}
--- app/web/admin_major.py
+++ app/web/admin_major.py
@@ -1,22 +1,57 @@
+from flask import jsonify, request
+
+from app.web.general import transform_errors
 from . import web
+from app.models.admin_major import MajorReader, SingleMajorReader, MajorUpdater, MajorInserter, MajorDeleter
+from app.validate.admin_major import AdminSelectSingleMajorForm, AdminUpdateMajorForm, AdminInsertMajorForm
 
 
-@web.route('/admin/major/select')
+@web.route('/admin/major/select', methods=['GET'])
 def admin_select_major():
+    major_reader = MajorReader()
+    return jsonify(major_reader.data), 404 if major_reader.data.get('error') else 200
     pass
 
 
-@web.route('/admin/major/insert')
-def admin_insert_major():
-    # 自动生成major_id
-    pass
-
-
-@web.route('/admin/major/update')
+@web.route('/admin/major/update', methods=['GET', 'POST'])
 def admin_update_major():
-    pass
+    if request.method == 'GET':
+        form = AdminSelectSingleMajorForm(request.args)
+        if form.validate():
+            major_id = form.major_id.data
+            major = SingleMajorReader(major_id)
+            return jsonify(major.data), 404 if major.data.get('error') else 200
+        else:
+            return jsonify(transform_errors(form.errors)), 404
+    else:
+        form = AdminUpdateMajorForm(request.args)
+        if form.validate():
+            major_id = form.major_id.data
+            major_name = form.major_name.data
+            updater = MajorUpdater(major_id, major_name)
+            return jsonify(updater.data), 404 if updater.data.get('error') else 200
+        else:
+            return jsonify(transform_errors(form.errors)), 404
 
 
-@web.route('/admin/major/delete')
+@web.route('/admin/major/insert', methods=['POST'])
+def admin_insert_major():
+    form = AdminInsertMajorForm(request.args)
+    if form.validate():
+        major_name = form.major_name.data
+        inserter = MajorInserter(major_name)
+        return jsonify(inserter.data), 404 if inserter.data.get('error') else 200
+    else:
+        return jsonify(transform_errors(form.errors)), 404
+
+
+@web.route('/admin/major/delete', methods=['POST'])
 def admin_delete_major():
-    pass
+    form = AdminSelectSingleMajorForm(request.args)
+    if form.validate():
+        major_id = form.major_id.data
+        deleter = MajorDeleter(major_id)
+        return jsonify(deleter.data), 404 if deleter.data.get('error') else 200
+    else:
+        return jsonify(transform_errors(form.errors)), 404
+
